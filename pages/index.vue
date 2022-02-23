ac04eaa88b4b8fae68c7a692f1d7fa79d489e4e8
ahmet <ca.ahmetcakir@gmail.com>
2022-02-23 22:59:53 +0300

    build

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,6 @@
         <v-card-text>
           <p>
             Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
             applications.
           </p>
           <p>
