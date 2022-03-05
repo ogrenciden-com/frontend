913f1f5e01b5d714151567ee64760d098b464d4e
ahmet <ca.ahmetcakir@gmail.com>
2022-03-05 18:24:10 +0300

    fix spacing problems in avatar-menu

--- components/AvatarMenu.vue
+++ components/AvatarMenu.vue
@@ -6,7 +6,7 @@
         outlined
         elevation="0"
         height="48"
-        class="rounded-lg px-1"
+        class="rounded-lg pr-3 pl-4"
         :style="{ lineHeight: '18px' }"
         v-bind="attrs"
         v-on="on"
