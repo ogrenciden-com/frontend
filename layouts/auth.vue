256cec8e29bcc5b03121bb93de1c8fd5f3435f89
ahmet <ca.ahmetcakir@gmail.com>
2022-03-11 20:05:25 +0300

    create auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-container fluid class="primary fill-height d-flex justify-center">
+      <Nuxt />
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'auth',
+}
+</script>
+
+<style >
+</style>
