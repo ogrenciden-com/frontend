4ba8bc7be22bf5ba6b28baf9d42eeee5cf8c95a0
ahmet <ca.ahmetcakir@gmail.com>
2022-03-13 15:39:20 +0300

    completed advert modal

--- components/AdvertModal.vue
+++ components/AdvertModal.vue
@@ -13,16 +13,17 @@
       </v-btn>
     </div>
     <form>
-      <v-row>
+      <v-row no-gutters>
         <v-col>
           <v-text-field
-            outlined
-            flat
-            solo
+            v-model="form.title"
             background-color="secondary"
             placeholder="İlan başlığı giriniz."
             height="42"
             color="darkGrey"
+            outlined
+            flat
+            solo
             dense
             hide-details
           ></v-text-field>
@@ -31,97 +32,190 @@
       <v-row>
         <v-col cols="4">
           <v-text-field
+            v-model="form.price"
             placeholder="Fiyat"
             append-icon="mdi-currency-try"
             type="number"
+            background-color="secondary"
+            color="darkGrey"
             reverse
             outlined
             flat
             hide-details
             hide-spin-buttons
             dense
-            background-color="secondary"
-            color="darkGrey"
           ></v-text-field>
         </v-col>
         <v-col cols="8">
-          <select-box label="Kategori" :outlined="true"></select-box>
-          <!-- <v-text-field
-            outlined
-            flat
-            solo
-            background-color="secondary"
-            placeholder="İlan başlığı giriniz."
-            height="42"
-            color="darkGrey"
-            dense
-            hide-details
-          ></v-text-field> -->
+          <select-box
+            :items="items"
+            v-model="form.category"
+            label="Kategori"
+            :outlined="true"
+            classes="mb-6"
+          ></select-box>
         </v-col>
       </v-row>
       <v-row no-gutters>
         <v-col>
-          <select-box label="Şehir" :outlined="true"></select-box>
+          <select-box
+            :items="items"
+            v-model="form.city"
+            label="Şehir"
+            :outlined="true"
+            classes="mb-6"
+          ></select-box>
         </v-col>
       </v-row>
       <v-row no-gutters>
         <v-col>
-          <select-box label="Üniversite" :outlined="true"></select-box>
+          <select-box
+            :items="university"
+            v-model="form.university"
+            label="Üniversite"
+            :outlined="true"
+            classes="mb-6"
+          ></select-box>
         </v-col>
       </v-row>
       <v-row no-gutters>
         <v-col>
-          <select-box label="Kampüs" :outlined="true"></select-box>
+          <select-box
+            :items="items"
+            v-model="form.campus"
+            label="Kampüs"
+            :outlined="true"
+            classes="mb-6"
+          ></select-box>
+          <!-- <v-select v-model="form.campus" :items="items"></v-select> -->
         </v-col>
       </v-row>
       <v-row no-gutters>
         <v-col>
           <v-text-field
+            v-model="form.tel"
             placeholder="(+90) İletişim bilgisi"
             type="number"
+            background-color="secondary"
+            color="darkGrey"
             outlined
             flat
             hide-details
             hide-spin-buttons
             dense
-            background-color="secondary"
-            color="darkGrey"
           ></v-text-field>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
           <v-textarea
+            v-model="form.description"
+            placeholder="İlan açıklaması"
+            background-color="white"
             auto-grow
             outlined
             flat
-            placeholder="İlan açıklaması"
-            background-color="white"
           ></v-textarea>
         </v-col>
       </v-row>
-      <v-btn
-        color="primary"
-        height="40"
-        width="160"
-        elevation="0"
-        class="font-weight-bold mb-8"
-        >Yayınla</v-btn
-      >
+      <v-row no-gutters>
+        <v-col class="d-flex justify-space-between mt-n2">
+          <v-sheet
+            v-for="(i, index) in 5"
+            :key="index"
+            width="110px"
+            height="110px"
+            color="secondary"
+            class="border"
+            outlined
+            flat
+            :style="{ position: 'relative' }"
+          >
+            <v-img
+              :src="form.url[index]"
+              height="110px"
+              width="110px"
+              cover
+            ></v-img>
+            <v-file-input
+              v-model="image"
+              class="centerCard"
+              accept="image/*"
+              prepend-icon="mdi-cloud-upload"
+              hide-input
+              @change="previewImage(index)"
+            >
+            </v-file-input>
+          </v-sheet>
+        </v-col>
+      </v-row>
+      <div class="d-flex justify-end">
+        <v-btn
+          color="primary"
+          height="40"
+          width="160"
+          elevation="0"
+          class="font-weight-bold my-6"
+          @click="submit"
+          >Yayınla</v-btn
+        >
+      </div>
     </form>
   </v-card>
 </template>
 <script>
 import { mapMutations } from 'vuex'
 import SelectBox from '@/components/SelectBox.vue'
+import UploadIcon from '@/components/Icons/UploadIcon.vue'
 export default {
   components: {
     SelectBox,
+    UploadIcon,
+  },
+  data() {
+    return {
+      form: {
+        title: '',
+        university: '',
+        campus: '',
+        description: '',
+        tel: '',
+        city: '',
+        category: '',
+        price: '',
+        url: [],
+      },
+      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+      image: [],
+    }
   },
   methods: {
     ...mapMutations({
       advertToggle: 'advertToggle',
     }),
+    previewImage(index) {
+      this.form.url[index] = URL.createObjectURL(this.image)
+    },
+    submit() {
+      console.log(this.form)
+    },
+  },
+  computed: {
+    university() {
+      return this.$store.state.university.list
+    },
   },
 }
 </script>
+
+<style scoped>
+.centerCard {
+  position: absolute;
+  top: calc(50% - 33px);
+  left: calc(50% - 10px);
+  z-index: 10;
+}
+.border {
+  border: 1px dashed #a6a2a2 !important;
+}
+</style>
