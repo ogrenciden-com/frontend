bb49cac8b5aba0ec3b075a87d1637d2621b2e42f
ahmet <ca.ahmetcakir@gmail.com>
2022-03-16 21:28:14 +0300

    add height attribute

--- components/SelectBox.vue
+++ components/SelectBox.vue
@@ -25,6 +25,10 @@ export default {
       type: String,
       default: '',
     },
+    height: {
+      type: [String, Number],
+      default: '',
+    },
     label: {
       type: String,
       default: '',
