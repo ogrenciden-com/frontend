625d91178be9a73623bb70258e6b748d6be9ee28
ahmet <ca.ahmetcakir@gmail.com>
2022-03-24 00:28:35 +0300

    install router-extra

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,12 +34,14 @@ export default {
 		// https://go.nuxtjs.dev/eslint
 		// '@nuxtjs/eslint-module',
 		// https://go.nuxtjs.dev/vuetify
+
 		'@nuxtjs/vuetify',
 	],
 
 	// Modules: https://go.nuxtjs.dev/config-modules
 	modules: [
 		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/router-extras',
 		'@nuxtjs/axios',
 	],
 
