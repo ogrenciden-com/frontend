4bdd3bad9068f86d7aa3d0fc01aa2488baf397e5
ahmet <ca.ahmetcakir@gmail.com>
2022-03-26 19:55:51 +0300

    decrease item card number

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 		<v-col order="last" order-md="first" cols="8" class="col-12 col-md-8">
 			<v-row>
 				<v-col
-					v-for="j in 100"
+					v-for="j in 21"
 					:key="j"
 					class="d-flex justify-center justify-sm-start"
 				>
