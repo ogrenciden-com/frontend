067dec14e192a0506006e920128a06937a920daa
ahmet <ca.ahmetcakir@gmail.com>
2022-03-27 14:28:21 +0300

    change link

--- components/FilterCard.vue
+++ components/FilterCard.vue
@@ -157,11 +157,10 @@ export default {
 			findCampusByUniversityName:
 				'UniversityAndCampus/findCampusByUniversityName',
 		}),
+		// linkleri store'a taşıdığında bunları sil
 		titleCase(str) {
 			let splitStr = str.toLowerCase().split('-')
 			for (let i = 0; i < splitStr.length; i++) {
-				// You do not need to check if i is larger than splitStr length, as your for does that for you
-				// Assign it back to the array
 				splitStr[i] =
 					splitStr[i].charAt(0).toUpperCase() +
 					splitStr[i].substring(1)
