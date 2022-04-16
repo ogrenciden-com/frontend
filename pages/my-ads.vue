910cb1f137a436aaa5c48311969107d22ebccaf5
ahmet <ca.ahmetcakir@gmail.com>
2022-04-17 00:33:20 +0300

    create my-ads page

---
+++ pages/my-ads.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h3 class="mb-4 text--subtitle">İlanlarım</h3>
+		<v-row>
+			<v-col
+				v-for="j in 6"
+				:key="j"
+				cols="3"
+				class="col-md-4 col-lg-3 col-sm-6 col-12 d-flex justify-center justify-md-start"
+			>
+				<item-card />
+			</v-col>
+		</v-row>
+	</div>
+</template>
+<script>
+import ItemCard from '@/components/ItemCard.vue'
+export default {
+	name: 'MyAds',
+	components: {
+		ItemCard,
+	},
+}
+</script>
