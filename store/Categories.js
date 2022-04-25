fdd4bd2ce8a2d25a3c1773d113749e9f90306d04
ahmet <ca.ahmetcakir@gmail.com>
2022-04-25 20:41:56 +0300

    refactor orderjs and create categories

---
+++ store/Categories.js
@@ -0,0 +1,40 @@
+export const state = () => ({
+	list: [
+		{
+			name: 'Ev',
+			slug: 'ev',
+		},
+		{
+			name: 'Buzdolabı',
+			slug: 'buzdolabi',
+		},
+		{
+			name: 'Çamaşır Makinesi',
+			slug: 'camasir-makinesi',
+		},
+		{
+			name: 'Yatak & Baza',
+			slug: 'yatak-baza',
+		},
+		{
+			name: 'Koltuk',
+			slug: 'koltuk',
+		},
+		{
+			name: 'Bulaşık Makinesi',
+			slug: 'bulasik-makinesi',
+		},
+		{
+			name: 'Çalışma Masası',
+			slug: 'calisma-masasi',
+		},
+		{
+			name: 'Dolap',
+			slug: 'dolap',
+		},
+		{
+			name: 'Diğer',
+			slug: 'diger',
+		},
+	],
+})
--- store/Order.js
+++ store/Order.js
@@ -17,25 +17,4 @@ export const state = () => ({
 			slug: 'fiyata-gore-azalan',
 		},
 	],
-	routeOrderName: undefined
 })
-
-export const mutations = {
-	findOrderBySlug(state, slug) {
-		state.list.forEach((order) => {
-			if (order.slug === slug) {
-				state.routeOrderName = order.name
-			}
-		})
-		return state.routeOrderName
-	},
-}
-export const getters = {
-	getOrdersName(state) {
-		const orders = []
-		for (const [key, value] of Object.entries(state.list)) {
-			orders.push(value.name)
-		}
-		return orders
-	},
-}
