39ffa562393c29e1db466179a7c04456b53e141f
ahmet <ca.ahmetcakir@gmail.com>
2022-05-24 17:47:53 +0300

    return modal page

--- store/index.js
+++ store/index.js
@@ -1,5 +1,6 @@
 export const state = () => ({
 	advertModal: false,
+	profileModal: false,
 	hasUser: true,
 })
 export const mutations = {
@@ -7,9 +8,9 @@ export const mutations = {
 		globalThis.$nuxt.$store.state.UniversityAndCampus.selectedCampuses = []
 		state.advertModal = !state.advertModal
 	},
-	// profileToggle() {
-	// 	this.$router.replace('/')
-	// },
+	profileToggle(state) {
+		state.profileModal = !state.profileModal
+	},
 	userToggle(state) {
 		state.hasUser = !state.hasUser
 	},
