4a01f51eb67097ef7faf7dac788e566424213cd2
ahmet <ca.ahmetcakir@gmail.com>
2022-05-28 23:27:18 +0300

    refactor and complete skeleton,alert,filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,49 @@
 <template>
 	<v-row>
-		<div v-if="showModal">
-			<nuxt-child></nuxt-child>
-		</div>
 		<v-col order="last" order-md="first" cols="8" class="col-12 col-md-8">
 			<v-row>
+				<template v-if="loading">
+					<v-col
+						v-for="index in 9"
+						:key="index"
+						cols="4"
+						class="pt-0 pb-6 pr-0 ml-n1 d-none d-sm-flex justify-center justify-md-start col-4 col-md-6 col-lg-4"
+					>
+						<card-skeleton />
+					</v-col>
+				</template>
 				<v-col
 					v-for="(ad, index) in ads"
+					v-else
 					:key="index"
 					cols="4"
 					class="pt-0 pb-6 pr-0 ml-n1 d-none d-sm-flex justify-center justify-md-start col-4 col-md-6 col-lg-4"
 				>
 					<item-card :ads="ad" />
 				</v-col>
+				<v-alert
+					v-if="err"
+					width="100%"
+					dense
+					outlined
+					type="error"
+					class="ml-3 mr-2"
+				>
+					Seçtiğiniz kriterlere ait ilan bulunamamıştır.
+				</v-alert>
 			</v-row>
+			<template v-if="loading">
+				<v-row
+					v-for="index in 5"
+					:key="index"
+					class="d-block d-sm-none px-3 pb-3"
+				>
+					<card-skeleton :card="false" />
+				</v-row>
+			</template>
 			<v-row
 				v-for="(ad, index) in ads"
+				v-else
 				:key="index"
 				class="d-block d-sm-none px-3 pb-3"
 			>
@@ -43,105 +71,54 @@
 import ItemCard from '@/components/ItemCard.vue'
 import ItemList from '@/components/ItemList.vue'
 import FilterCard from '@/components/FilterCard.vue'
+import CardSkeleton from '@/components/CardSkeleton.vue'
 export default {
 	name: 'IndexPage',
 	components: {
 		ItemCard,
 		ItemList,
 		FilterCard,
+		CardSkeleton,
 	},
 	data() {
 		return {
-			// ads: [
-			// 	{
-			// 		title: 'Apple Orjinal Şarj Başlığı',
-			// 		price: '120TL',
-			// 		category: 'Teknoloji',
-			// 		campus: 'Farabi',
-			// 		image: 'https://img.letgo.com/images/2a/00/6f/f1/2a006ff155483ebdf0a951ad50a993fe.jpg?impolicy=img_384',
-			// 	},
-			// 	{
-			// 		title: 'HP Yazıcı Ve Fotokopi',
-			// 		price: '290 TL',
-			// 		category: 'Teknoloji',
-			// 		campus: 'Farabi',
-			// 		image: 'https://img.letgo.com/images/6a/45/ec/72/6a45ec724d7a590e94f80dc942b35a29.jpg?impolicy=img_384',
-			// 	},
-			// 	{
-			// 		title: 'KONSOL',
-			// 		price: '1.750 TL',
-			// 		category: 'Dolap',
-			// 		campus: 'İbn-i Sina',
-			// 		image: 'https://img.letgo.com/images/bd/a1/90/9f/bda1909f28150ffaeaa5004ecd389348.jpeg?impolicy=img_384',
-			// 	},
-			// 	{
-			// 		title: 'SALON TAKIMI İMALATTAN ŞOOK KAMPANYA',
-			// 		price: '4.999 TL',
-			// 		category: 'Koltuk',
-			// 		campus: 'Çaycuma',
-			// 		image: 'https://img.letgo.com/images/b5/74/f0/8a/b574f08acf886c4159739b4d781767b1.jpeg?impolicy=img_384',
-			// 	},
-			// 	{
-			// 		title: '4 Kapaklı Dolap',
-			// 		price: '800 TL',
-			// 		category: 'Dolap',
-			// 		campus: 'Farabi',
-			// 		image: 'https://img.letgo.com/images/83/bc/87/d8/83bc87d817df9284b5633a22b6a18ebe.jpeg?impolicy=img_384',
-			// 	},
-			// 	{
-			// 		title: 'BEKO 8 KG A+ DİGİTAL 9 PROGRAM LI ÇOK TEMİZ ÇAMAŞIR MAKİNASI',
-			// 		price: '3.500 TL',
-			// 		category: 'Çamaşır Makinası',
-			// 		campus: 'Farabi',
-			// 		image: 'https://img.letgo.com/images/9d/a1/d0/e1/9da1d0e1295ca0a06c6aec72925babd5.jpeg?impolicy=img_384',
-			// 	},
-			// 	{
-			// 		title: 'ÇİFT KİŞİLİK DERİ BAZA BAŞLIĞI 1.50',
-			// 		price: '140 TL',
-			// 		category: 'Yatak & Baza',
-			// 		campus: 'İbn-i Sina',
-			// 		image: 'https://img.letgo.com/images/07/6b/8d/23/076b8d23a551a1c6983ccada325249dc.jpeg?impolicy=img_384',
-			// 	},
-			// 	{
-			// 		title: '4 Metre Kare',
-			// 		price: '300 TL',
-			// 		category: 'Diğer',
-			// 		campus: 'Çaycuma',
-			// 		image: 'https://img.letgo.com/images/f0/6f/91/2f/f06f912f191b76221e7e1eec230de6bd.jpeg?impolicy=img_384',
-			// 	},
-			// 	{
-			// 		title: 'Hp Pavilion G6 Ssd Diskli Ekran Kartlı İş Laptopu',
-			// 		price: '2.750 TL',
-			// 		category: 'Teknoloji',
-			// 		campus: 'Farabi',
-			// 		image: 'https://img.letgo.com/images/8f/29/ab/a7/8f29aba7f36736100e47db849c0585e1.jpeg?impolicy=img_384',
-			// 	},
-			// ],
 			ads: [],
+			loading: false,
+			err: '',
 		}
 	},
-	computed: {
-		showModal() {
-			return this.$route.matched.length
-		},
+	async fetch() {
+		await this.getProducts()
 	},
 	mounted() {
-		// console.log(this.$auth.user)
 		this.$vuetify.goTo(0)
-	},
-	async created() {
-		await this.getProducts()
 	},
 	methods: {
 		async getProducts() {
 			try {
-				const res = await this.$axios.$post('/products/filter')
-
-				// eslint-disable-next-line no-console
+				this.loading = true
+				const res = await this.$axios.$post('/products/filter', {
+					text: this.$route.query.text || undefined,
+					category: this.$route.params.category || undefined,
+					university: this.$route.params.university || undefined,
+					campus: this.$route.params.campus || undefined,
+					maxPrice: this.$route.query.maxPrice || undefined,
+					minPrice: this.$route.query.minPrice || undefined,
+					sortPrice:
+						this.$route.query?.order?.length <= 4
+							? this.$route.query?.order
+							: undefined,
+					sortDate:
+						this.$route.query?.order?.length > 4
+							? this.$route.query?.order
+							: undefined,
+				})
 				this.ads = res
 			} catch (err) {
-				// eslint-disable-next-line no-console
-				this.$nuxt.error({ err })
+				if (err.response.status > 404) return this.$nuxt.error({ err })
+				this.err = err.response.data.message
+			} finally {
+				this.loading = false
 			}
 		},
 	},
